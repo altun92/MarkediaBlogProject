fdf8ba3a412054a2d37263c337f6d6689775963c
anlal <anlal@ANL>
2024-12-25 23:17:53 +0300

    Add project files.

---
+++ MarkediaBlogProject.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,23 @@
+﻿using MarkediaBlogProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkediaBlogProject.DataAccessLayer.Context
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ANıL;initial catalog=MarkediaBlogDb;integrated security=true");
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+    }
+}
---
+++ MarkediaBlogProject.EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkediaBlogProject.EntityLayer.Concrete
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Status { get; set; }
+
+        public int ArticleId { get; set; }
+        public Article Articles { get; set; }
+    }
+}
